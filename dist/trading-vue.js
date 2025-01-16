0e7ba2a1a1591853f44d4370b612d81439fe00a5
shrutip0511 <shruti.p@covrize.com>
2025-01-16 18:26:18 +0530

    added style for pointer

--- dist/trading-vue.js
+++ dist/trading-vue.js
@@ -443,7 +443,7 @@ module.exports = exports;
 
 /***/ }),
 
-/***/ 257:
+/***/ 248:
 /***/ ((module, exports, __webpack_require__) => {
 
 // Imports
@@ -4241,19 +4241,19 @@ if(false) {}
 
 /***/ }),
 
-/***/ 452:
+/***/ 897:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 // style-loader: Adds some css to the DOM by adding a <style> tag
 
 // load the styles
-var content = __webpack_require__(257);
+var content = __webpack_require__(248);
 if(content.__esModule) content = content.default;
 if(typeof content === 'string') content = [[module.id, content, '']];
 if(content.locals) module.exports = content.locals;
 // add the styles to the DOM
 var add = (__webpack_require__(534)/* ["default"] */ .A)
-var update = add("2a4eb95c", content, false, {});
+var update = add("8180622a", content, false, {});
 // Hot Module Replacement
 if(false) {}
 
@@ -11878,8 +11878,8 @@ var Sidebar_component = normalizeComponent(
 )
 
 /* harmony default export */ const components_Sidebar = (Sidebar_component.exports);
-;// ./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader/lib/loaders/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/components/Legend.vue?vue&type=template&id=3062f892
-var Legendvue_type_template_id_3062f892_render = function render() {
+;// ./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader/lib/loaders/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/components/Legend.vue?vue&type=template&id=261ea969
+var Legendvue_type_template_id_261ea969_render = function render() {
   var _vm$common;
   var _vm = this,
     _c = _vm._self._c;
@@ -11922,6 +11922,9 @@ var Legendvue_type_template_id_3062f892_render = function render() {
     }
   }, [_vm._v("\n            " + _vm._s((_vm.common.meta.last || [])[4]) + "\n        ")]) : _vm._e()], 2) : _vm._e(), _vm._v(" "), _vm.grid_id === 0 && _vm.isOverlayCollapsed ? _c('button', {
     staticClass: "p-button p-component p-button-secondary",
+    staticStyle: {
+      "cursor": "pointer"
+    },
     attrs: {
       "type": "button"
     },
@@ -11974,6 +11977,9 @@ var Legendvue_type_template_id_3062f892_render = function render() {
     }) : _vm._e()], 1)], 1) : _vm._e();
   }), _vm._v(" "), _vm.grid_id === 0 && !_vm.isOverlayCollapsed ? _c('button', {
     staticClass: "p-button p-component p-button-secondary",
+    staticStyle: {
+      "cursor": "pointer"
+    },
     attrs: {
       "type": "button"
     },
@@ -11986,10 +11992,10 @@ var Legendvue_type_template_id_3062f892_render = function render() {
     staticClass: "pi pi-angle-up p-button-icon"
   })]) : _vm._e()], 2);
 };
-var Legendvue_type_template_id_3062f892_staticRenderFns = [];
-Legendvue_type_template_id_3062f892_render._withStripped = true;
+var Legendvue_type_template_id_261ea969_staticRenderFns = [];
+Legendvue_type_template_id_261ea969_render._withStripped = true;
 
-;// ./src/components/Legend.vue?vue&type=template&id=3062f892
+;// ./src/components/Legend.vue?vue&type=template&id=261ea969
 
 ;// ./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader/lib/loaders/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/components/ButtonGroup.vue?vue&type=template&id=72b6dd45
 var ButtonGroupvue_type_template_id_72b6dd45_render = function render() {
@@ -12353,9 +12359,9 @@ var settingPng = icons_namespaceObject["gear.png"];
 });
 ;// ./src/components/Legend.vue?vue&type=script&lang=js
  /* harmony default export */ const components_Legendvue_type_script_lang_js = (Legendvue_type_script_lang_js); 
-// EXTERNAL MODULE: ./node_modules/vue-style-loader/index.js!./node_modules/css-loader/dist/cjs.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/components/Legend.vue?vue&type=style&index=0&id=3062f892&prod&lang=css
-var Legendvue_type_style_index_0_id_3062f892_prod_lang_css = __webpack_require__(452);
-;// ./src/components/Legend.vue?vue&type=style&index=0&id=3062f892&prod&lang=css
+// EXTERNAL MODULE: ./node_modules/vue-style-loader/index.js!./node_modules/css-loader/dist/cjs.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/components/Legend.vue?vue&type=style&index=0&id=261ea969&prod&lang=css
+var Legendvue_type_style_index_0_id_261ea969_prod_lang_css = __webpack_require__(897);
+;// ./src/components/Legend.vue?vue&type=style&index=0&id=261ea969&prod&lang=css
 
 ;// ./src/components/Legend.vue
 
@@ -12368,8 +12374,8 @@ var Legendvue_type_style_index_0_id_3062f892_prod_lang_css = __webpack_require__
 
 var Legend_component = normalizeComponent(
   components_Legendvue_type_script_lang_js,
-  Legendvue_type_template_id_3062f892_render,
-  Legendvue_type_template_id_3062f892_staticRenderFns,
+  Legendvue_type_template_id_261ea969_render,
+  Legendvue_type_template_id_261ea969_staticRenderFns,
   false,
   null,
   null,
--- src/components/Legend.vue
+++ src/components/Legend.vue
@@ -36,7 +36,7 @@
       </legend-button> -->
         </div>
         <button type="button" class="p-button p-component p-button-secondary"
-            v-if="(grid_id === 0 && isOverlayCollapsed)" @click="collapse_button_click(false)">
+            v-if="(grid_id === 0 && isOverlayCollapsed)" @click="collapse_button_click(false)" style="cursor: pointer;">
 
             <span class="pi pi-angle-down p-button-icon p-button-icon-left"></span>
             <span class="p-button-label">{{ this.indicators.length }}</span>
@@ -61,7 +61,7 @@
                 </spinner>
             </transition>
         </div>
-        <button type="button" class="p-button p-component p-button-secondary" v-if="(grid_id === 0 && !isOverlayCollapsed)" @click="collapse_button_click(true)">
+        <button type="button" class="p-button p-component p-button-secondary" v-if="(grid_id === 0 && !isOverlayCollapsed)" @click="collapse_button_click(true)" style="cursor: pointer;">
 
             <span class="pi pi-angle-up p-button-icon"></span>
         </button>
